e8aa7fb17b0428891bd02fc91a3276e9f7f788a8
Gowtham245211 <gautamlatha47@gmail.com>
2021-07-17 15:47:17 +0530

    Payilagam Excercise:4

---
+++ SouthIndia.java
@@ -0,0 +1,13 @@
+abstract class SouthIndia extends India {
+
+   void cultivate() {
+	   
+	    System.out.println("Rice and Wheat Cultivation");
+   }
+	
+   void livingStyle() {
+	
+	    System.out.println("Average Development");
+   }
+	
+}
