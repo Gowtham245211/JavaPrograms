1319305099fa7bea060d89e43f4ba8ba43cdf158
Gowtham245211 <gautamlatha47@gmail.com>
2021-08-07 22:40:30 +0530

    Identifying whether the given number is a multiple of 3 and 5

---
+++ FizzBuzz.java
@@ -0,0 +1,36 @@
+package com.payilagam.Programs;
+import java.util.Scanner;
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a number :");
+		
+		int input = sc.nextInt();
+		
+		sc.nextLine();
+		
+		if(input%3==0 && input%5==0) {
+			
+			System.out.println("FizzBuzz");
+		}
+		
+		else if(input%3==0) {
+			System.out.println("Fizz");
+		}
+		else if(input%5==0) {
+			System.out.println("Buzz");
+		}
+		else {
+			System.out.println(input);
+		}
+		
+		sc.close();
+		
+
+	}
+
+}
